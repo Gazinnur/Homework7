46a0fb5ee618d4dd71146b483f6656eb730df565
gazinnur <gazinnur@mail.ru>
2022-11-02 14:36:29 +0300

    add exp 004

---
+++ Homework7/Exp004/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N. 
+Console.WriteLine("Введите число");
+int Num = int.Parse(Console.ReadLine());
+int biginNum = 1;
+while (biginNum <= Num)
+{
+    if (biginNum % 2 == 0)
+    {
+        Console.WriteLine(biginNum );
+     }
+biginNum=biginNum+1;
+}
