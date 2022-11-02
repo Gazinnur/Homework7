39b9ce37a18df4c2f62303df2fb80ca6878458d3
gazinnur <gazinnur@mail.ru>
2022-11-02 13:42:06 +0300

    add exp 003

---
+++ Homework7/Exp003/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Введите число");
+int Num= int.Parse(Console.ReadLine());
+
+if (Num>=0)
+{
+    Console.WriteLine(Num +" положительное");
+}
+else
+Console.WriteLine(Num +" отрицательное");
